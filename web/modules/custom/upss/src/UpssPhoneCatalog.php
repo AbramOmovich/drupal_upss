f9b2c126d5c42b5c2ab2c5e0dbf9403bc44fa52c
Roman Tselepnev <Roman_Tselepnev@epam.com>
2017-11-13 16:03:36 +0300

    phone images

--- web/modules/custom/upss/src/UpssPhoneCatalog.php
+++ web/modules/custom/upss/src/UpssPhoneCatalog.php
@@ -7,7 +7,9 @@ class UpssPhoneCatalog {
   private const FOLDER = __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR .
         'mock_data' . DIRECTORY_SEPARATOR . 'phones' . DIRECTORY_SEPARATOR;
 
-  function getPhones(int $page = NULL) : array {
+  private const IMG_DIR = 'public://phone_images/';
+
+  public function getPhones(int $page = NULL) : array {
     $dir = opendir(self::FOLDER);
     $phones = [];
 
@@ -26,6 +28,8 @@ class UpssPhoneCatalog {
       if ($i >= $start && $i < $end){
         $file = file_get_contents(self::FOLDER . $file);
         $phone = unserialize($file);
+        $phone['image'] = $this->getPhoneImageUrl($phone);
+        unset($phone['images']);
         $phones []= $phone;
       }
       $i++;
@@ -34,7 +38,7 @@ class UpssPhoneCatalog {
     return $phones;
   }
 
-  function getNumberOfPhones() : int {
+  public function getNumberOfPhones() : int {
     $dir = opendir(self::FOLDER);
     $amount = 0;
 
@@ -46,5 +50,10 @@ class UpssPhoneCatalog {
     }
 
     return $amount;
+  }
+
+  private function getPhoneImageUrl(array $phone): string
+  {
+    return self::IMG_DIR . $phone['id'] . '.' . pathinfo($phone['images']['header'], PATHINFO_EXTENSION);
   }
 }
